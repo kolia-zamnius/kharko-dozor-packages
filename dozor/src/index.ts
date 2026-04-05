b624fc3c31a91f8a4eb6be20efb6e142f1043302
kolia-zamnius <kolzam.person@gmail.com>
2026-04-05 11:48:00 +0300

    feat(dozor): export public API

---
+++ dozor/src/index.ts
@@ -0,0 +1,12 @@
+export { Dozor } from "./recorder";
+export type { Logger } from "./recorder/logger";
+export type {
+  DozorOptions,
+  DozorState,
+  IngestPayload,
+  SessionMetadata,
+  SliceMarker,
+  SliceReason,
+  UserIdentity,
+  UserTraits,
+} from "./types";
