3ac37da76b1b37bf5ef5bfdfe10a64c0ac13eb6e
kolia-zamnius <kolzam.person@gmail.com>
2026-04-18 10:40:00 +0300

    feat(react): export public API

---
+++ dozor-react/src/index.ts
@@ -0,0 +1,3 @@
+export { DozorProvider } from "./provider";
+export type { DozorActions, DozorContextState, DozorContextValue, DozorSnapshot } from "./types";
+export { useDozor } from "./use-dozor";
